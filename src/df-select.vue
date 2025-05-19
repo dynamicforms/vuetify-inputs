a8d73fb68bc59e7c93965f6d97069a1c605186e9
Jure Erznožnik <jure@velis.si>
2025-05-19 13:16:36 +0200

    - df-select reactive choices & value

--- src/df-select.vue
+++ src/df-select.vue
@@ -71,7 +71,7 @@
 <script setup lang="ts">
 import { DisplayMode } from '@dynamicforms/vue-forms';
 import { unionBy } from 'lodash-es';
-import { ref, computed, toRefs, watch, nextTick } from 'vue';
+import { ref, computed, toRefs, watch, nextTick, unref } from 'vue';
 import IonIcon from 'vue-ionicon';
 
 import {
@@ -108,8 +108,7 @@ const propsWithDefaults = withDefaults(defineProps<Props>(), {
   fetchChoices: undefined,
 });
 
-const { choices } = propsWithDefaults;
-const { multiple, allowTags: taggable, allowNull, cssClass, visibility } = toRefs(propsWithDefaults);
+const { choices, multiple, allowTags: taggable, allowNull, cssClass, visibility } = toRefs(propsWithDefaults);
 
 interface Emits extends BaseEmits {
   (e: 'update:modelValueDisplay', value: SelectChoice[]): any;
@@ -124,7 +123,9 @@ defineSlots<{
 const { errors, label, touched, value: resultingValue, vuetifyBindings } = useInputBase(propsWithDefaults, emits);
 
 const selected = ref<any>(null);
-const loadedChoices = ref<SelectChoice[]>(choices || []);
+const takeLoaded = ref(false);
+const loaded = ref<SelectChoice[]>([]);
+const loadedChoices = computed<SelectChoice[]>(() => (unref(takeLoaded) ? unref(loaded) : unref(choices) || []));
 const loading = ref<boolean>(false);
 
 const options = computed(() => convertItems(loadedChoices.value));
@@ -133,20 +134,33 @@ function emitModelValueDisplay(mcVal: any) {
   emits('update:modelValueDisplay', getSelectedChoices(loadedChoices.value, mcVal));
 }
 
+let setResultingValueGuard = false;
+function setResultingValue(newValue: any) {
+  setResultingValueGuard = true;
+  resultingValue.value = newValue;
+  nextTick(() => { setResultingValueGuard = false; });
+}
+
 watch(selected, (newValue) => {
   if (vuetifyBindings.value.readonly) return;
   nextTick(() => {
     const mcVal = multipleCompliantValue(newValue, multiple.value);
     emitModelValueDisplay(mcVal);
-    resultingValue.value = mcVal;
+    setResultingValue(mcVal);
   });
+}, { deep: true });
+watch(resultingValue, (newValue: any) => {
+  if (!setResultingValueGuard) {
+    const mcVal = multipleCompliantValue(newValue, multiple.value);
+    updateSelectedFromValue(mcVal, selected, multiple.value, false, loadedChoices.value);
+  }
 }, { deep: true });
 
 function onSelect(/* val: any */) {
   if (vuetifyBindings.value.readonly || taggable.value) return;
   const mcVal = multipleCompliantValue(selected.value, multiple.value);
   updateSelectedFromValue(mcVal, selected, multiple.value, false, loadedChoices.value);
-  resultingValue.value = mcVal;
+  setResultingValue(mcVal);
 }
 
 function chipClose(itemValue: any) {
@@ -157,17 +171,18 @@ function chipClose(itemValue: any) {
     mcVal = null;
   }
   updateSelectedFromValue(mcVal, selected, multiple.value, false, loadedChoices.value);
-  resultingValue.value = mcVal;
+  setResultingValue(mcVal);
 }
 
 async function queryOptions(queryValue?: any, idValue?: any): Promise<void> {
-  if (choices || propsWithDefaults.fetchChoices === undefined) return;
+  if (choices.value || propsWithDefaults.fetchChoices === undefined) return;
   loading.value = true;
   try {
     const selectedChoices =
       getSelectedChoices(loadedChoices.value, multipleCompliantValue(selected.value, multiple.value));
     const newChoices = await propsWithDefaults.fetchChoices(queryValue, idValue);
-    loadedChoices.value = unionBy([...selectedChoices, ...newChoices], 'id');
+    loaded.value = unionBy([...selectedChoices, ...newChoices], 'id');
+    takeLoaded.value = true;
   } finally {
     loading.value = false;
   }
@@ -182,7 +197,7 @@ function initialValueCheck() {
   val = multipleCompliantValue(val, multiple.value);
   updateSelectedFromValue(val, selected, multiple.value, taggable.value, loadedChoices.value);
   emitModelValueDisplay(val);
-  resultingValue.value = val;
+  setResultingValue(val);
 }
 
 initialValueCheck();
